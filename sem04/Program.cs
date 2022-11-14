0ce16786611e1dc8e0debbbfca4be982a0871add
andrei <afonuk@mail.ru>
2022-11-14 22:50:40 +0300

    4rd task

---
+++ sem04/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным.
+int a = Int32.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
+
