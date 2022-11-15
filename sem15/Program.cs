1274124be636b87b95e8b195b9a744b06f543171
andrei <afonuk@mail.ru>
2022-11-15 20:56:28 +0300

    add 12 and 15 tasks

---
+++ sem15/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+n = Math.Abs(n);
+// if 
+// (n < 100) System.Console.WriteLine("No");
+// else
+// System.Console.WriteLine(n/100%10);
+if (n > 99) {
+    while (n > 1000) {
+        n = n / 10;
+    }
+    System.Console.WriteLine(n % 10);
+} else {
+    System.Console.WriteLine("No");
+}
