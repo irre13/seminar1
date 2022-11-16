c9e4e5fe148aa6211fedf0c2f4d35decf3d7a95e
andrei <afonuk@mail.ru>
2022-11-16 22:15:40 +0300

    add task14

---
+++ sem14/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+if ( a % b == 0) {
+    System.Console.WriteLine("Yes");
+} else {
+    System.Console.WriteLine(a % b);
+}
