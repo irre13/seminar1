ae9ffc41e16dc7a2d4a66e5dc8ac5961535198fe
andrei <afonuk@mail.ru>
2022-11-20 19:42:34 +0300

    add task 16 and 31

---
+++ sem16/Program.cs
@@ -0,0 +1,6 @@
+﻿// Дано число.Проверить кратно ли оно 7 и 23
+int a = Int32.Parse(Console.ReadLine());
+if((a % 7 == 0) || (a % 23 == 0)){
+    Console.WriteLine("YES");
+}
+else Console.WriteLine("NO");
---
+++ sem31/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Random random = new Random(10);
+
+for(int i = 0; i < 5; i++) {
+    int a = random.Next(1, 100);
+    int b = Math.Pow(a,3);
+    if (b % 10 % 2 == 0);
+    System.Console.WriteLine($"{a}, {b}");
+}
+    
+
+// System.Console.WriteLine(random.Next(1, 100)+" ");
