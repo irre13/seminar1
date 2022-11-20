55d98d7709b2222b73ca2f752b4db9b3363ffb69
andrei <afonuk@mail.ru>
2022-11-20 19:54:08 +0300

    add task 17

---
+++ sem17/Program.cs
@@ -0,0 +1,7 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+int a = Int32.Parse(Console.ReadLine());
+if (a == 6 || a == 7) {
+    Console.WriteLine("Yes");
+} else {
+    Console.WriteLine("No");
+}
