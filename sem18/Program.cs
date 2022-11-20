8527c29905b58f631997fa58276d3b1f10abe2b8
andrei <afonuk@mail.ru>
2022-11-20 20:01:02 +0300

    add task 18

---
+++ sem18/Program.cs
@@ -0,0 +1,7 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого.
+int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+if (a * a == b || b * b == a){
+    Console.WriteLine("Yes");
+}
+else Console.WriteLine("No");
