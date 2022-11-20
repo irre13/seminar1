2f487d3783ec41200af238ee1704400a6c3a9f69
andrei <afonuk@mail.ru>
2022-11-20 23:06:42 +0300

    add 22task on js)

---
+++ sem22/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+function Palindrom(num) {
+    return Number(num.toString().split("").reverse().join("")) === num;
+}
+console.log(Palindrom(12321));
+ЭТО на JS );
